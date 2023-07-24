19b1964230f179dc87f0f9746ca4c912b64303bc
Shivganesh <shivmarkad99@gmail.com>
2023-07-24 10:28:11 +0530

    [Shivganesh] Created signUp page for New User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,53 +1,9 @@
 import HttpStatus from 'http-status-codes';
 import * as UserService from '../services/user.service';
 
-/**
- * Controller to get all users available
- * @param  {object} req - request object
- * @param {object} res - response object
- * @param {Function} next
- */
-export const getAllUsers = async (req, res, next) => {
+export const signUp = async (req, res, next) => {  //signup
   try {
-    const data = await UserService.getAllUsers();
-    res.status(HttpStatus.OK).json({
-      code: HttpStatus.OK,
-      data: data,
-      message: 'All users fetched successfully'
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-/**
- * Controller to get a single user
- * @param  {object} req - request object
- * @param {object} res - response object
- * @param {Function} next
- */
-export const getUser = async (req, res, next) => {
-  try {
-    const data = await UserService.getUser(req.params.id);
-    res.status(HttpStatus.OK).json({
-      code: HttpStatus.OK,
-      data: data,
-      message: 'User fetched successfully'
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-/**
- * Controller to create a new user
- * @param  {object} req - request object
- * @param {object} res - response object
- * @param {Function} next
- */
-export const newUser = async (req, res, next) => {
-  try {
-    const data = await UserService.newUser(req.body);
+    const data = await UserService.signUp(req.body);
     res.status(HttpStatus.CREATED).json({
       code: HttpStatus.CREATED,
       data: data,
@@ -58,53 +14,16 @@ export const newUser = async (req, res, next) => {
   }
 };
 
-/**
- * Controller to update a user
- * @param  {object} req - request object
- * @param {object} res - response object
- * @param {Function} next
- */
-export const updateUser = async (req, res, next) => {
-  try {
-    const data = await UserService.updateUser(req.params.id, req.body);
-    res.status(HttpStatus.ACCEPTED).json({
-      code: HttpStatus.ACCEPTED,
-      data: data,
-      message: 'User updated successfully'
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
-/**
- * Controller to delete a single user
- * @param  {object} req - request object
- * @param {object} res - response object
- * @param {Function} next
- */
-export const deleteUser = async (req, res, next) => {
-  try {
-    await UserService.deleteUser(req.params.id);
-    res.status(HttpStatus.OK).json({
-      code: HttpStatus.OK,
-      data: [],
-      message: 'User deleted successfully'
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const userValidator = async (req, res, next)=>{
-  try {
-    const data = await UserService.userValidator(req.body.email, req.body.password);
-    res.status(HttpStatus.OK).json({
-      code : HttpStatus.OK,
-      data : data.email,
-     message: "User Login successfully"
-    });
-  } catch(error){
-    next(error);
-  }
-}
+// export const signIn = async (req, res, next)=>{    //signin
+//   try {
+//     const data = await UserService.signIn(req.body.email, req.body.password);
+//     res.status(HttpStatus.OK).json({
+//       code : HttpStatus.OK,
+//       data : data.email,
+//      message: "User Login successfully"
+//     });
+//   } catch(error){
+//     next(error);
+//   }
+// }
