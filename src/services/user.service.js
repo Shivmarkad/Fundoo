fcc52f458fd01f9139f6fcf4742d460e22d594d8
Shivganesh <shivmarkad99@gmail.com>
2023-07-25 16:25:37 +0530

    [Shivganesh] Added JWT token for user when login successfully

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,29 +1,38 @@
 import { result } from '@hapi/joi/lib/base';
 import sequelize, { DataTypes } from '../config/database';
 const User = require('../models/user')(sequelize, DataTypes);
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+// dotenv.config();
 
 //signUp for new user
 export const signUp = async (body) => {
+  const user = await User.findOne({ where: { email: body.email } });
+  if (user) {
+    throw new Error("You are already registered!!");
+  };
   const saltRounds = 10;
   const hash = bcrypt.hashSync(body.password, saltRounds);
   body.password = hash;
   const data = await User.create(body);
-  return data;
+  return data.email;
 };
 
 //signIn registered user
 export const signIn = async (email, password) => {
 
-  const data = await User.findOne({ where: { email: email } })
-  if(data == null){
-    return "user not found";
-  }
+  const data = await User.findOne({ where: { email: email } });
+  if (data == null) {
+    throw new Error("user not found");
+  } ;
   const hash = data.password;
   const isTrue = bcrypt.compareSync(password, hash);
-  if(isTrue){
-    return data.email;
-  }else{
-    return "Login failed incorrect password";
+
+  if (isTrue) {
+    var token = jwt.sign({ foo: 'bar' },process.env.SECRET_KEY);
+    return token;
+  } else {
+    throw new Error("Password Incorrect");
   }
 }
