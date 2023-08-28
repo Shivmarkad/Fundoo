aa4ddbec1b2434aaea259e1aa4a8266015dd54a6
Shivganesh <shivmarkad99@gmail.com>
2023-08-29 02:52:01 +0530

    [Shivganesh]Refactored: Refactored istrash and isarchive API's

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -5,7 +5,7 @@ export const getAllNotes = async (req) => {
   const note = await Note.find({ createdBy: req.body.createdBy });
   if (note) {
     const noteData = JSON.stringify(note);
-    await client.set(req.body.createdBy,noteData);
+    await client.set(req.body.createdBy, noteData);
     return note;
   };
   throw new Error("Unable to find");
@@ -22,7 +22,7 @@ export const createNote = async (body) => {
 };
 
 export const findNoteById = async (id, userId) => {
-  const note = await Note.findOne({  _id: id, createdBy: userId});
+  const note = await Note.findOne({ _id: id, createdBy: userId });
   if (note) {
     return note;
   };
@@ -30,8 +30,8 @@ export const findNoteById = async (id, userId) => {
 };
 
 export const updateNoteById = async (body, id) => {
-  const update = {title: body.title, description: body.description }
-  const note = await Note.findByIdAndUpdate({createdBy:body.createdBy,_id:id},update, {new:true});
+  const update = { title: body.title, description: body.description }
+  const note = await Note.findByIdAndUpdate({ createdBy: body.createdBy, _id: id }, update, { new: true });
   if (note) {
     await client.del(body.createdBy);
     return note;
@@ -40,7 +40,7 @@ export const updateNoteById = async (body, id) => {
 };
 
 export const deleteNoteById = async (id, userId) => {
-  const note = await Note.findOneAndDelete({_id: id, createdBy: userId});
+  const note = await Note.findOneAndDelete({ _id: id, createdBy: userId });
   if (note) {
     await client.del(userId);
     return note;
@@ -52,12 +52,24 @@ export const isArchieve = async (id, body) => {
   const archieveNote = await Note.findOne({ _id: id, createdBy: body.createdBy });
   let archieveStatus;
   archieveNote.isArchieve ? archieveStatus = false : archieveStatus = true;
-  await Note.findByIdAndUpdate({ id: id, createdBy: body.createdBy},{ isArchieve: archieveStatus },{new:true});
-};        
+  const note = await Note.findByIdAndUpdate({ _id: id, createdBy: body.createdBy }, { isArchieve: archieveStatus }, { new: true });
+  if (note) {
+    await client.del(body.createdBy);
+    return note;
+  }
+  throw new Error("Unable to archieve");
+
+};
 
 export const isTrash = async (id, body) => {
-  const trashNote = await Note.findOne({ _id: id, createdBy: body.createdBy});
+  const trashNote = await Note.findOne({ _id: id, createdBy: body.createdBy });
   let trashStatus;
   trashNote.isTrash ? trashStatus = false : trashStatus = true;
-  const note = await Note.findByIdAndUpdate({ id: id, createdBy: body.createdBy},{ isTrash: trashStatus },{new:true});
+  const note = await Note.findByIdAndUpdate({ _id: id, createdBy: body.createdBy }, { isTrash: trashStatus }, { new: true });
+  if (note) {
+    await client.del(body.createdBy);
+    return note;
+  }
+  throw new Error("Unable to delete note");
+
 };
--- src/validators/notes.validator.js
+++ src/validators/notes.validator.js
@@ -6,6 +6,8 @@ export const notesValidator = (req, res, next) => {
     title: Joi.string().min(3).required(),
     description: Joi.string().min(3).required(),
     color: Joi.string().min(3).optional(),
+    isArchieve: Joi.boolean().optional(),
+    isTrash: Joi.boolean().optional(),
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
