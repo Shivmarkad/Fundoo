826d2efe25fa9bbeac4ea97647c005179d89ff6c
Shivganesh <shivmarkad99@gmail.com>
2023-09-06 23:17:36 +0530

    [Shivganesh] Refactor : to be readable

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -1,46 +1,34 @@
-import { where } from 'sequelize';
 import sequelize, { DataTypes } from '../config/database';
-import notes from '../models/notes';
 
 const Notes = require('../models/notes')(sequelize, DataTypes);
 
 export const getAllNotes = async (req) => {
   const note = await Notes.findAll({ where: { createdBy: req.body.createdBy } });
-  if (note) {
-    return note;
-  };
+  if (note) { return note; };
   throw new Error("Unable to find");
 };
 
 export const createNote = async (body) => {
   const note = await Notes.create(body);
-  if (note) {
-    return note;
-  };
+  if (note) { return note };
   throw new Error("unable to create note");
 };
 
 export const findNoteById = async (id, req) => {
   const note = await Notes.findOne({ where: { id: id, createdBy: req.body.createdBy } });
-  if (note) {
-    return note;
-  };
+  if (note) { return note; };
   throw new Error("Unable to find the note");
 };
 
 export const updateNoteById = async (body, id) => {
   const note = await Notes.update({ title: body.title, description: body.description }, { where: { id: id, createdBy: body.createdBy } });
-  if (note) {
-    return note;
-  };
+  if (note) { return note; };
   throw new Error("Unable to update the note");
 };
 
 export const deleteNoteById = async (id, body) => {
   const note = await Notes.destroy({ where: { id: id, createdBy: body.createdBy } });
-  if (note) {
-    return note;
-  };
+  if (note) { return note; };
   throw new Error("Unable to delete note");
 };
 
@@ -55,5 +43,5 @@ export const isTrash = async (id, body) => {
   const trashNote = await Notes.findOne({ where: { id: id, createdBy: body.createdBy } });
   let trashStatus;
   trashNote.isTrash ? trashStatus = false : trashStatus = true;
-  const note = await Notes.update({ isTrash: trashStatus }, { where: { id: id, createdBy: body.createdBy } });
+  await Notes.update({ isTrash: trashStatus }, { where: { id: id, createdBy: body.createdBy } });
 };
